860a7fbc8aa726ef11539cae918547dd7122f6d8
Jon Knopp <owner@jonknopp.com>
2024-04-23 21:48:08 -0600

    refactor: move responseWasCached function to metrics module
    - Remove responseWasCached function from helpers.js
    - Add responseWasCached function to metrics.js
    - Adjust imports in loadstorm.js for lodash and htmlReport to
      maintain consistent ordering

--- loadstorm.js
+++ loadstorm.js
@@ -1,9 +1,9 @@
 import http from 'k6/http'
 import { group,  sleep } from 'k6'
 import { rand, sitemapUrls, getPage } from './lib/helpers.js'
-import _ from 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js'
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import _ from 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js'
 import Metrics from './lib/metrics.js';
 
 const metrics = new Metrics()
